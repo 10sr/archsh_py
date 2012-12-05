72390f5cbe2ab424152d62af5f868398ffa15784
10sr <8slashes+git@gmail.com>
2012-12-05 18:49:34 +0900

    add complete_cd met

--- archsh/prompt.py
+++ archsh/prompt.py
@@ -17,18 +17,15 @@ else:
         readline.parse_and_bind("tab: complete")
 
 class ArchCmd(Cmd) :
-    _cand = []                  # candidate for completer
-    _buf = ""                   # current buffer of readline
-    _arg = []                   # splitted self._buf
-
     def __init__(self, env, execute) :
-        Cmd.__init__(self)
-        self.intro = "Archsh command line for archive"
         self._env = env
         self._exec = execute
         # if readline :
         #     readline.set_completer_delims(" ")
         #     readline.set_completer(self._completer)
+        Cmd.__init__(self)
+        self.intro = "Archsh command line for archive"
+        self.prompt = "%s:%s $ " % (self._env.file, self._env.cwd)
         return
 
     def completedefault(self, text, line, begidx, endidx) :
@@ -44,56 +41,46 @@ class ArchCmd(Cmd) :
         cand = [(head + e) for e in current if e.startswith(tail)]
         return cand
 
-    def _completer(self, text, state) :
-        def com_filter(array, text) :
-            return [s for s in array if s.startswith(text)]
-
-        if state == 0 :
-            # i feel like get_line_buffer() is broken, it holds previous
-            # buffer when input is empty yet.
-            endidx = readline.get_endidx()
-            buf = readline.get_line_buffer()[:endidx]
-            if buf == "" :
-                self._cand = []
-            else :
-                arg = shsplit(buf)
-                if len(arg) == 0 :
-                    curdir = self.env.cwd
-                else :
-                    curdir = self.env.get_dir(arg[-1]) or self.env.cwd
-                child, current = self.env.get_current_list(curdir)
-                self._cand = com_filter(current, text)
-
-        try :
-            return self._cand[state]
-        except IndexError :
-            return None
+    def complete_cd(self, text, line, begidx, endidx) :
+        args = shsplit(line)
+        if len(args) >= 3 :
+            return []
+        else :
+            cand = self.completedefault(text, line, begidx, endidx)
+            cand = [e for e in cand if e.endswith("/")]
+            return cand
 
     def postcmd(self, stop, line) :
-        self.prompt = "cmd %s : " % line
+        self.prompt = "%s:%s $ " % (self._env.file, self._env.cwd)
         return stop
 
     def emptyline(self) :
         return False
 
     def do_exit(self, line) :
-        print("")
         print("Exit.")
         return True
 
-    do_EOF = do_exit
+    def do_EOF(self, line) :
+        print("")
+        return self.do_exit(line)
 
     def do_cd(self, line) :
-        """Change current directory."""
+        """cd: Change current directory."""
         args = self._parse_line(line)
         print(args)
         self._exec.run_cd(args)
         return False
 
     def do_ls(self, line) :
-        """List current directory files."""
+        """ls: List current directory files."""
         args = self._parse_line(line)
         self._exec.run_ls(args)
+        return False
+
+    def do_pwd(self, line) :
+        """pwd: Print current working directory."""
+        print(self._env.cwd)
         return False
 
     def _parse_line(self, line) :
