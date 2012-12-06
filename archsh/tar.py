81d511f38c58710c7cec57cf94504faff7b59955
10sr <8slashes+git@gmail.com>
2012-12-06 23:34:04 +0900

    update handler

--- archsh/tar.py
+++ archsh/tar.py
@@ -22,39 +22,47 @@ class ArchHandler() :
 
 class TAR(ArchHandler) :
     suffixes = [".tar"]
-    list_command = ["tar", "-tf"]
-    cat_command = ["tar", "-xOf"]
-    extract_command = ["tar", "-xf"]
+
+    tar_command = "tar"
+    file_option = "-f"
+    directory_option = "-C"
+    cat_option = "-O"
+
+    list_option = "-t"
+    extract_option = "-x"
 
     def get_list(self) :
-        lst = check_output(self.list_command + [self.file]).decode().split("\n")
+        lst = check_output([self.tar_command, self.list_option,
+                            self.file_option, self.file]).decode().split("\n")
         self.list = [e for e in lst if e != ""]
         return self.list
 
     def cat_files(self, files) :
         r = []
         for f in files :
-            p = Popen(self.cat_command + [self.file, f],
+            p = Popen([self.tar_command, self.extract_option, self.cat_option,
+                       self.file_option, self.file, f],
                       stdout=PIPE, stderr=STDOUT)
             r.append((f, p.stdout))
         return r
 
     def open_files(self, files, tempdir) :
         lfiles = list(files)
-        call(self.extract_command + [self.file, "-C", tempdir] + lfiles)
+        call([self.tar_command, self.extract_option, self.file_option,
+              self.file, self.directory_option, tempdir] + lfiles)
         return [(f, osjoin(tempdir, f)) for f in files]
 
 class TGZ(TAR) :
     suffixes = [".tar.gz", ".tgz"]
-    list_command = ["tar", "-tzf"]
-    cat_command = ["tar", "-xzOf"]
+    list_option = "-tz"
+    extract_option = "-xz"
 
 class TBZ(TAR) :
     suffixes = [".tar.bz2", ".tbz"]
-    list_command = ["tar", "-tjf"]
-    cat_command = ["tar", "-xjOf"]
+    list_option = "-tj"
+    extract_option = "-xj"
 
 class TXZ(TAR) :
     suffixes = [".tar.xz", ".txz"]
-    list_command = ["tar", "-tJf"]
-    cat_command = ["tar", "-xJOf"]
+    list_option = "-tJ"
+    extract_option = "-xJ"
