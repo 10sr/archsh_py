0d15030c9826408262fad5a99de7b4d69d3b9de2
10sr <8slashes+git@gmail.com>
2012-12-12 16:59:48 +0900

    add setup.py

--- archsh/__init__.py
+++ archsh/__init__.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 
+"""archsh - shell for Archive
+
+Cd to your archive files and execute commands.
+"""
+
 from .shell import Shell as shell
+
+__version__ = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+import archsh
+
+setup(name = "archsh",
+      version = archsh.__version__,
+      description = "Shell for Archive",
+      long_description = archsh.__doc__,
+      author = "10sr",
+      author_email = "sr10@sourceforge.org",
+      url = "https://github.com/10sr/archsh_py",
+      download_url = "https://github.com/10sr/archsh_py/archive/master.zip",
+      packages = ["archsh"],
+      scripts = ["bin/archsh"],
+      keywords = "archive utility",
+      classifiers=['License :: Public Domain']
+      )
