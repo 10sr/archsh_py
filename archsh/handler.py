6e7249865fe14ad06ec8725fd7a72e4b38ff7059
10sr <8slashes+git@gmail.com>
2012-12-21 20:51:23 +0900

    add class FileInfo

--- archsh/handler.py
+++ archsh/handler.py
@@ -5,6 +5,7 @@ Base class for archive Handler.
 """
 
 class Handler() :
+    """Base class for archive handler."""
     suffixes = []
     def __init__(self, file) :
         self.file = file
@@ -12,8 +13,18 @@ class Handler() :
     def get_list(self) :
         return []
     def cat_files(self, files) :
-        """Return list of tuple (file, output), where output is file object."""
+        """Return list of tuple (file, output),
+
+        output is file object."""
         return []
     def open_files(self, files, tempdir) :
-        """Return list of tuple (file, path), path is where the file created."""
+        """Return list of tuple (file, path).
+
+        path is where the file was created."""
         return []
+
+class FileInfo() :
+    def __init__(self, name, mdate) :
+        self.name = name
+        self.mdate = mdate
+        return
