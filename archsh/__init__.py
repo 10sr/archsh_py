36319b05f98bdedd467b054a3002b85cde4ba34d
10sr <8slashes+git@gmail.com>
2013-01-29 17:49:30 +0900

    increase version

--- archsh/__init__.py
+++ archsh/__init__.py
@@ -2,9 +2,9 @@
 
 """archsh - shell for Archive
 
-Cd to your archive files and execute commands.
+Cd to your archive files and execute commands like ls or less.
 """
 
 from archsh.shell import Shell as shell
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
